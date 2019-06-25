3bdd67ec5cc7f69350668aaeffaf944ee27631ac
Yangjun Wang <yangjunw50@outlook.com>
2019-06-25 23:18:18 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
--- Function that makes users tired unless they gain enough sleep.
-nutrition.tired = {}
+-- Init and registration
 hb.register_hudbar("nutrition",0xFFFFFF,"Nutrition",,12,12,false)
+
+-- Init hudbar for new users
+minetest.register_on_joinplayer(function(player)
+hb.init_hudbar(player,"nutrition",12)
+)
