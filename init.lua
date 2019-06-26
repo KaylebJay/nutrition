60eb0bcc45617f433fd1223dbfccce5c90e079eb
Yangjun Wang <yangjunw50@outlook.com>
2019-06-26 13:40:36 +0800

    Nutrition -> Tiredness; Simple counter (to be completed)

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 -- Init and registration
-hb.register_hudbar("nutrition",0xFFFFFF,"Nutrition",,12,12,false)
+hb.register_hudbar("tiredness",0xFFFFFF,"Tiredness",,12,12,false)
 
 -- Init hudbar for new users
 minetest.register_on_joinplayer(function(player)
-hb.init_hudbar(player,"nutrition",12)
+hb.init_hudbar(player,"tiredness",12)
 )
 
 -- Chatcommand: take a break
@@ -12,12 +12,28 @@ minetest.register_chatcommand("break",{
   description="Take a break and continue working",
   func=function(name,param)
     if not mineest.get_player_by_name(name) then return end
-    minetest.show_formspec(minetest.get_player_by_name(name),"nutrition:breakform",nutrition.breakform)
+    nutrition.breakform(minetest.get_player_by_name(name))
   end,
 })
 
 -- Privilege
-minetest.register_privilege("nobreak","Can survive without taking a break")
+minetest.register_privilege("nobreak","Can survive without getting tired")
 
 -- A simple formspec that makes users click to take a "break"
-nutrition.breakform = "size[4,3]button_exit[1,1;2,1;exit;Take a break]"
+nutrition.breakform = function(player)
+  minetest.show_formspec(player,"nutrition:breakform","size[4,3]button_exit[1,1;2,1;exit;Take a break]")
+  hb.change_hudbar(player,"tiredness",12)
+end
+
+-- Counter: makes the user more tired by increasing "tiredness" every 10 mins (600s)
+-- I don't know what the opposite of "tiredness" is. If you know, please tell me.
+local timer=0
+minetest.register_globalstep(function(dtime)
+  timer = timer + dtime;
+  if timer >= 600 then
+  timer = 0
+  for _,player in ipairs(minetest.get_connected_players()) do
+    -- To be continued (I'm qiite busy at the moment).
+  end
+  end
+end)
