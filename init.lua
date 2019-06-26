853fd336a6533b353ca2ef819940b622c287a0f6
Yangjun Wang <yangjunw50@outlook.com>
2019-06-26 13:18:31 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,3 +5,19 @@ hb.register_hudbar("nutrition",0xFFFFFF,"Nutrition",,12,12,false)
 minetest.register_on_joinplayer(function(player)
 hb.init_hudbar(player,"nutrition",12)
 )
+
+-- Chatcommand: take a break
+minetest.register_chatcommand("break",{
+  params="",
+  description="Take a break and continue working",
+  func=function(name,param)
+    if not mineest.get_player_by_name(name) then return end
+    minetest.show_formspec(minetest.get_player_by_name(name),"nutrition:breakform",nutrition.breakform)
+  end,
+})
+
+-- Privilege
+minetest.register_privilege("nobreak","Can survive without taking a break")
+
+-- A simple formspec that makes users click to take a "break"
+nutrition.breakform = "size[4,3]button_exit[1,1;2,1;exit;Take a break]"
