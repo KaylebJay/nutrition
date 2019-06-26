723f256f4649d1d6c81445ec0883ab918a5edb90
Yangjun Wang <yangjunw50@outlook.com>
2019-06-26 21:17:18 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -21,8 +21,8 @@ minetest.register_privilege("health_bypass","Can always survive without health p
 
 -- A simple formspec that makes users click to take a "break"
 nutrition.breakform = function(player)
-  minetest.show_formspec(player,"nutrition:breakform","size[4,3]button_exit[1,1;2,1;exit;Take a break]")
-  hb.change_hudbar(player,"tiredness",12)
+  minetest.show_formspec(player,"nutrition:breakform","size[3,3]button[0.5,0.5;2,1;break;Take a break]button_exit[0.5,1.5;2,1;exit;Done]")
+  hb.change_hudbar(player,"tiredness",hb.get_hudbar_state(player,"tiredness").value+1)
 end
 
 -- Counter: makes the user more tired by increasing "tiredness" every 10 mins (600s)
@@ -34,7 +34,7 @@ minetest.register_globalstep(function(dtime)
   timer = 0
   for _,player in ipairs(minetest.get_connected_players()) do
     if not minetest.get_player_privs(player:get_player_name()).health_bypass then
-      hb.change_hudbar(player,"tiredness",hb.get_hudbar_state(player,"tiredness").value-1)
+      if not hb.get_hudbar_state(player,"tiredness").value==0 then hb.change_hudbar(player,"tiredness",hb.get_hudbar_state(player,"tiredness").value-1) end
       if hb.get_hudbar_state(player,"tiredness").value==0 then nutrition.breakform(player) end
     end
   end
